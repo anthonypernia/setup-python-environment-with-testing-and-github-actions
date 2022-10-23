1a406cafcd311a1873b26ddbb7689c1f651dd115
Anthony Pernia <anthonyperniah@gmail.com>
2022-10-23 19:21:13 -0300

    modif classes

--- tests_example/test_example.py
+++ tests_example/test_example.py
@@ -1,6 +1,5 @@
 """EXAMPLE TEST"""
 from example_class.example_class_file import ExampleClass
-from example_class.example_class_file_second import ExampleClassSecond
 
 
 class TestExampleClass:
@@ -20,12 +19,12 @@ class TestExampleClass:
 
     def test_is_palindrome(self):
         """EXAMPLE TEST 2"""
-        assert ExampleClassSecond().is_palindrome("racecar") is True
+        assert ExampleClass("example").is_palindrome("racecar") is True
 
     def test_is_prime(self):
         """EXAMPLE TEST 2"""
-        assert ExampleClassSecond().is_prime(7) is True
+        assert ExampleClass("example").is_prime(7) is True
 
     def test_is_even(self):
         """EXAMPLE TEST 2"""
-        assert ExampleClassSecond().is_even(4) is True
+        assert ExampleClass("example").is_even(4) is True
